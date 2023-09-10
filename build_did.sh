c74db86ed471df73100c8b5edec95ed3edb53de8
newbmiao <newbvirgil@gmail.com>
2023-09-10 13:24:06 +0800

    add rust target

--- build_did.sh
+++ build_did.sh
@@ -2,6 +2,15 @@
 
 package="dvote_backend"
 did_file="src/$package/$package.did"
+# install rustup and cargo 
+if ! command -v rustup &>/dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+if ! command -v cargo &>/dev/null; then
+    source "$HOME/.cargo/env"
+fi
+
+rustup target add wasm32-unknown-unknown
 cargo build --target wasm32-unknown-unknown \
     --release \
     --package "$package" --features "ic-cdk/wasi"
